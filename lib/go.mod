7d4e100aae49ffcaba8af37410b9cb973a87ccb1
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-21 23:36:02 +0300

    Added Store to lib

--- lib/go.mod
+++ lib/go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.9.1
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.2.1
 	github.com/aws/aws-sdk-go-v2/service/cloudwatch v1.8.1
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.5.1
 	github.com/bogdanrat/aws-serverless-poc/contracts v0.0.0-20210921200834-9246f54b28a4
 )
---
+++ lib/store/store.go
@@ -0,0 +1,15 @@
+package store
+
+import "github.com/bogdanrat/aws-serverless-poc/contracts/models"
+
+const (
+	TitleTableAttributeName    = "Title"
+	AuthorTableAttributeName   = "Author"
+	CategoryTableAttributeName = "Category"
+	FormatsTableAttributeName  = "Formats"
+)
+
+type Store interface {
+	GetAll() ([]*models.Book, error)
+	PutMany(books []*models.Book) error
+}
