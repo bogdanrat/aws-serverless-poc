26315d0edfde2754117e103b34cd37305245d8b6
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-22 12:36:44 +0300

    Added Env var

--- contracts/common/env.go
+++ contracts/common/env.go
@@ -3,4 +3,5 @@ package common
 const (
 	BooksTableNameEnvironmentVariable         = "TABLE_NAME"
 	BooksCategoryIndexNameEnvironmentVariable = "TABLE_CATEGORY_INDEX_NAME"
+	BooksTitleIndexNameEnvironmentVariable    = "TABLE_TITLE_INDEX_NAME"
 )
