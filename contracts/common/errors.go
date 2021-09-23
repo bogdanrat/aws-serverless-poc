979c575b630b20e37300f77c250147949679fc1e
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-23 10:06:49 +0300

    Added: Vars

--- contracts/common/errors.go
+++ contracts/common/errors.go
@@ -3,10 +3,11 @@ package common
 import "errors"
 
 var (
-	MethodNotAllowedErr = errors.New("method not allowed")
-	InvalidPayloadErr   = errors.New("invalid payload")
-	DynamoDBActionErr   = errors.New("dynamodb error")
-	CWLogsErr           = errors.New("could not log request")
-	MarshalBooksErr     = errors.New("error marshalling books")
-	SNSPublishErr       = errors.New("publish sns error")
+	MethodNotAllowedErr  = errors.New("method not allowed")
+	InvalidPayloadErr    = errors.New("invalid payload")
+	DynamoDBActionErr    = errors.New("dynamodb error")
+	DynamoDBUnmarshalErr = errors.New("error unmarshalling dynamodb items")
+	CWLogsErr            = errors.New("could not log request")
+	MarshalBooksErr      = errors.New("error marshalling books")
+	SNSPublishErr        = errors.New("publish sns error")
 )
