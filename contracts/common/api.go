58e5fbbd9dfde2aa8c583aea4c58ff08359c7010
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-23 09:50:38 +0300

    Added: Consts

--- contracts/common/api.go
+++ contracts/common/api.go
@@ -1,9 +1,23 @@
 package common
 
+import "errors"
+
 const (
 	HttpGetMethod    = "get"
 	HttpPostMethod   = "post"
 	HttpPutMethod    = "put"
 	HttpPatchMethod  = "patch"
 	HttpDeleteMethod = "delete"
+)
+
+const (
+	ContentTypeHeader          = "Content-Type"
+	ContentTypeApplicationJSON = "application/json"
+)
+
+var (
+	MethodNotAllowedErr = errors.New("method not allowed")
+	InvalidPayloadErr   = errors.New("invalid payload")
+	DynamoDBActionErr   = errors.New("dynamodb error")
+	CWLogsErr           = errors.New("could not log request")
 )
