dbfc30d64ada77758c026f075129b68ac6f456c8
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-23 10:00:25 +0300

    Added: Vars

--- contracts/common/env.go
+++ contracts/common/env.go
@@ -5,4 +5,5 @@ const (
 	BooksTableNameEnvironmentVariable         = "TABLE_NAME"
 	BooksCategoryIndexNameEnvironmentVariable = "TABLE_CATEGORY_INDEX_NAME"
 	BooksTitleIndexNameEnvironmentVariable    = "TABLE_TITLE_INDEX_NAME"
+	BooksTopicArnEnvironmentVariable          = "BOOKS_TOPIC_ARN"
 )
