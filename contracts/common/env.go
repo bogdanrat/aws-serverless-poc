4160b4cf8cbf8fcf4231d95b8f6acbd2e70ef3ce
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-23 08:37:45 +0300

    Added: AWSRegionEnvironmentVariable

--- contracts/common/env.go
+++ contracts/common/env.go
@@ -1,6 +1,7 @@
 package common
 
 const (
+	AWSRegionEnvironmentVariable              = "REGION"
 	BooksTableNameEnvironmentVariable         = "TABLE_NAME"
 	BooksCategoryIndexNameEnvironmentVariable = "TABLE_CATEGORY_INDEX_NAME"
 	BooksTitleIndexNameEnvironmentVariable    = "TABLE_TITLE_INDEX_NAME"
