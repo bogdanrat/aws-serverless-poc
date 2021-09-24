4057db0144f5de028c7c6b4b635750f56cd2f75b
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-24 20:37:01 +0300

    Build script

--- build.sh
+++ build.sh
@@ -2,33 +2,57 @@
 
 DEPLOYMENT_PACKAGE_NAME=deployment.zip
 
-echo "Building binaries..."
-cd functions/get-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+while getopts ":f:a" opt; do
+  case $opt in
+    f)
+      echo "building binary for $OPTARG function..." >&2
 
-cd ../create-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+      cd functions/"$OPTARG" && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
 
-cd ../update-book && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+      ;;
+    a)
+      echo "building binaries for all functions..." >&2
 
-cd ../delete-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+      cd functions/get-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
 
-cd ../search-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+      cd ../create-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
 
-cd ../stream-processor && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+      cd ../update-book && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
 
-cd ../auth && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
-zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
-rm main
+      cd ../delete-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
+
+      cd ../search-books && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
+
+      cd ../stream-processor && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
+
+      cd ../auth && GOOS=linux GOARCH=amd64 go build -o main cmd/main.go || exit
+      zip ${DEPLOYMENT_PACKAGE_NAME} main || exit
+      rm main
+
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 echo "Done."
