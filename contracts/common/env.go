2388cfdbc57698670a5238ad6df05c2a5c2a0584
Bogdan Raț <bogdan.rat@zenitech.co.uk>
2021-09-24 13:36:07 +0300

    Added: env vars

--- contracts/common/env.go
+++ contracts/common/env.go
@@ -6,4 +6,6 @@ const (
 	BooksCategoryIndexNameEnvironmentVariable = "TABLE_CATEGORY_INDEX_NAME"
 	BooksTitleIndexNameEnvironmentVariable    = "TABLE_TITLE_INDEX_NAME"
 	BooksTopicArnEnvironmentVariable          = "BOOKS_TOPIC_ARN"
+	CognitoUserPoolIDEnvironmentVariable      = "COGNITO_USER_POOL_ID"
+	CognitoAppClientIDEnvironmentVariable     = "COGNITO_APP_CLIENT_ID"
 )
